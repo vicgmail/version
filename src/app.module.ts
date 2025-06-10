1a105dd5dc62da39b0b6a64528b1e7866ab3ca03
Vic <victorlymar@gmail.com>
2025-06-10 14:34:09 +0300

    ssl setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,9 @@ import { VersionModule } from './version/version.module';
         force: false,
       },
       logging: process.env.DB_SQL_LOG == '1',
+      dialectOptions: {
+        ssl: process.env.DB_SSL === '1',
+      },
     }),
     VersionModule,
   ],
