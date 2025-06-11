81cef9be3ac4fadd905ac8c581fca29bc650008e
Vic <victorlymar@gmail.com>
2025-06-11 15:41:28 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ENV } from './constants/env';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
